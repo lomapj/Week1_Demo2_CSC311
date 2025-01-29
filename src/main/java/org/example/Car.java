39707fc0e3aa54236f56fa3c64ef62643ad37ec2
Patrick Lomangino <lomapj@farmingdale.edu>
2025-01-29 10:26:00 -0500

    2nd commit

---
+++ src/main/java/org/example/Car.java
@@ -0,0 +1,30 @@
+package org.example;
+
+public class Car extends Vehicle {
+    private String brand;
+
+    public Car(int numberOfWheels, String color, float engineSize, String fuelType, String brand) {
+        super(numberOfWheels, color, engineSize, fuelType);
+        this.brand = brand;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public void honk() {
+        System.out.println("Honk, honk!");
+    }
+
+    public void displayInfo() {
+        System.out.println("Brand: " + brand);
+        System.out.println("Number of Wheels: " + getNumberOfWheels());
+        System.out.println("Color: " + getColor());
+        System.out.println("Engine Size: " + getEngineSize());
+        System.out.println("Fuel Type: " + getFuelType());
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,10 @@
 package org.example;
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+        Car myCar = new Car(4, "Black", 2.0f, "Gasoline", "Hynudai");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        myCar.honk();
+        myCar.displayInfo();
     }
 }
+
